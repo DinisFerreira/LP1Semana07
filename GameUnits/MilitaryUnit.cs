596a2034748c03b4d63415760b8ccf72892b1a42
Dinis <dinisvferreira@gmail.com>
2024-04-19 10:43:58 +0100

    add militaryunit changes

--- GameUnits/MilitaryUnit.cs
+++ GameUnits/MilitaryUnit.cs
@@ -1,14 +1,37 @@
+using System;
+
 namespace GameUnits
 {
-    public class MilitaryUnit : Unit // Heranca!!
+    public class MilitaryUnit : Unit // Herança!!
     {
         public int AttackPower { get; }
         public int XP { get; private set; }
 
+        // Health property é calculada adicionando XP ao valor base.
+        public override int Health
+        {
+            get => base.Health + XP;
+            set => base.Health = value;
+        }
+
+        // Cost property é calculada somando AttackPower e XP.
+        public override float Cost => AttackPower + XP;
+
         public MilitaryUnit(int mov, int health, int attackPower) : base(mov, health)
-        {                                                        // Unit(int, int)
+        {
             AttackPower = attackPower;
             XP = 0;
         }
+
+        // Método de ataque. Incrementa a XP e reduz a saúde da unidade alvo.
+        public void Attack(Unit u)
+        {
+            XP++;
+            u.Health -= AttackPower;
+        }
+
+        // Método ToString que retorna uma representação em string dos atributos da unidade.
+        public override string ToString() =>
+            $"{GetType().Name}: HP={Health} COST={Cost:F2} AP={AttackPower} XP={XP}";
     }
 }
